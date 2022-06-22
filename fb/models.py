8879b8ee52d94135e2799b460d5be9013eab9105
badhonbarman <ithesbc@gmail.com>
2022-06-23 00:03:58 +0600

    backend structure dev1

--- fb/models.py
+++ fb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+	name = models.CharField(max_length=100, default='')
+	password = models.CharField(max_length=200)
+
+	def register(self):
+		self.save()
--- fb/views.py
+++ fb/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import*
 
 def index_page(request):
+
+	user_name= request.POST.get("user_name")
+	user_pass= request.POST.get("user_pass")
+
+
+	user_data = user(name=user_name,password=user_pass)
+	user_data.register()
+	print(user_name)
+
 	return render(request,"index.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
