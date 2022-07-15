88491160c50375cb55097c994826995dd1eb26c0
badhonbarman <ithesbc@gmail.com>
2022-07-15 21:25:27 +0600

    sign up form added

--- fb/static/js/custom.js
+++ fb/static/js/custom.js
@@ -1,13 +1,21 @@
+for (var i = 1; i <= 31; i++) {
+    $('#birthDay').append('<option value="' + i + '">' + i + '</option>');
+}
+
+for (var i = 1; i <= 12; i++) {
+    $('#birthMonth').append('<option value="' + i + '">' + i + '</option>');
+}
+
+var currentTime = new Date();
+var year = currentTime.getFullYear();
+for (var i = year; i >= 1900; i--) {
+    $('#birthYear').append('<option value="' + i + '">' + i + '</option>');
+}
+
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
 })
 
 
@@ -26,3 +34,4 @@ $(document).on('submit','#post_form', function(e){
 		}
 	})
 })
+
