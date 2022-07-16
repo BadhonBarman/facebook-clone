b9007a9b21b00ae65f9c98c00832973f4b7cf91d
badhonbarman <ithesbc@gmail.com>
2022-07-16 19:30:52 +0600

    sign up form on proccess

--- fb/static/js/custom.js
+++ fb/static/js/custom.js
@@ -1,16 +1,14 @@
-for (var i = 1; i <= 31; i++) {
+
+	for (var i = 1; i <= 31; i++) {
     $('#birthDay').append('<option value="' + i + '">' + i + '</option>');
-}
+	}
 
-for (var i = 1; i <= 12; i++) {
-    $('#birthMonth').append('<option value="' + i + '">' + i + '</option>');
-}
 
-var currentTime = new Date();
-var year = currentTime.getFullYear();
-for (var i = year; i >= 1900; i--) {
-    $('#birthYear').append('<option value="' + i + '">' + i + '</option>');
-}
+	var currentTime = new Date();
+	var year = currentTime.getFullYear();
+	for (var i = year; i >= 1905; i--) {
+	    $('#birthYear').append('<option value="' + i + '">' + i + '</option>');
+	}
 
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
