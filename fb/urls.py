8996cd1e0326adefd0dd5083ae3617ba7d62532b
badhonbarman <ithesbc@gmail.com>
2022-07-19 22:52:27 +0600

    singup funtion update

--- fb/urls.py
+++ fb/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index_page, name='index'),
     # path('signup', views.signup, name='signup'),
     path('profile', views.user_profile, name='profile'),
+    path('signup', views.SignUp, name='signup'),
     path('home', views.home, name='home')
 ]
--- fb/views.py
+++ fb/views.py
@@ -10,36 +10,36 @@ def index_page(request):
 		#log in info
 		user_phone= request.POST.get("user_phone_number")
 		user_pass= request.POST.get("user_pass")
-		if user_phone:
-			get_user= User.login_user_by_phone(user_phone)
+		error_txt =None
+		get_user= User.login_user_by_phone(user_phone)
 
-			if get_user:
-				request.session['name']=get_user.user_name
-				request.session['phone']=get_user.user_phone
-				return redirect('profile') 
+		if get_user:
+			request.session['name']=get_user.user_name
+			request.session['phone']=get_user.user_phone
+			return redirect('profile') 
 		else:
-			# sign up info 
-			user_firstname = request.POST.get("firstname")
-			user_surname = request.POST.get("surname")
-			# full name
-			user_fullname= user_firstname+user_surname
-			user_mobile  = request.POST.get("mobile")
-			user_signuppass = request.POST.get("signuppass")
-			# Date of birth
-			birthDay= request.POST.get("birthday")
-			birthMonth= request.POST.get("birthmonth")
-			birthYear= request.POST.get("birthyear")
-			# full date of birth
-			user_dateOFbirth= birthDay+ birthMonth+ birthYear
+			print("restriction working")
 
-			user_gender=request.POST.get("gender")
-
-			print(user_dateOFbirth)
-			# store sign up data to DB
-			new_user_data=User(user_name=user_fullname,user_phone=user_mobile,user_signuppass=user_signuppass,user_dateOFbirth=user_dateOFbirth,user_gender=user_gender)
-			new_user_data.register()
-
-		return render(request,"index.html")
+def SignUp(request):
+	#sign up info 
+	user_firstname = request.POST.get("firstname")
+	user_surname = request.POST.get("surname")
+	# full name
+	user_fullname= user_firstname+user_surname
+	user_mobile  = request.POST.get("mobile")
+	user_signuppass = request.POST.get("signuppass")
+	# Date of birth
+	birthDay= request.POST.get("birthday")
+	birthMonth= request.POST.get("birthmonth")
+	birthYear= request.POST.get("birthyear")
+	# full date of birth
+	user_dateOFbirth= birthDay+ birthMonth+ birthYear
+	user_gender=request.POST.get("gender")
+	print(user_dateOFbirth)
+	# store sign up data to DB
+	new_user_data=User(user_name=user_fullname,user_phone=user_mobile,user_signuppass=user_signuppass,user_dateOFbirth=user_dateOFbirth,user_gender=user_gender)
+	new_user_data.register()
+	return redirect('index')
 
 
 
